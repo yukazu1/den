b6d406856db641c6622015d957e9eae9750b8544
uenokaisei <96405848+uenokaisei@users.noreply.github.com>
2022-06-03 08:29:03 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+
+    function update( _v ) // input tag　を更新する関数
+    {
+      document. querySelector( 'input' ).value = _v
+    }
+    
+    
+    function append ( _v)//数字ボタンが押されたので数字を後ろに追加する
+    {
+      document. querySelector( 'input' ). value += _v
+    }
+
+    
+   function sss ( _v)//ルートだから計算させるっす
+    {
+      document. querySelector( 'input' ). value += _v
+      const v = document. querySelector( 'input' ).value
+      try{
+        const f = new Function( 'return ' + v )
+        update( f().toString() )
+        } catch( _error ){}
+          
+    } 
+       function mmm ( _v)//ルートだから計算させるっす
+    {
+        const v = document. querySelector( 'input' ).value
+      try{
+        const f = new Function( 'return ' + v )
+        update( f().toString() )
+        } catch( _error ){}
+          
+      document. querySelector( 'input' ). value += _v
+    
+    } 
+
+    function  calc() // 「＝」が押されたので計算する
+    {
+      const v = document. querySelector( 'input' ).value
+      try{
+        const f = new Function( 'return ' + v )
+        update( f().toString() )
+        } catch( _error ){}
+          
+        
+    }
